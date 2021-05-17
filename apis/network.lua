990acb8d71693f16a54bb40e29ab0a4b7e1d77cc
yelsharawy <yelsharawy20@stuy.edu>
2021-05-17 16:44:43 -0400

    Fixed network API filter

--- apis/network.lua
+++ apis/network.lua
@@ -1,8 +1,14 @@
-craftyTurtle = peripheral.find("turtle", function (_, obj) return obj.getLabel() == "crafter" end)
+local function filterCrafter(_, obj)
+    return obj.getLabel() == "crafter" end
+end
+local function filterBinnie(_, obj)
+    return obj.getMetadata().name == "binniecore:storage"
+end
+craftyTurtle = peripheral.find("turtle", filterCrafter)
 craftingStation = peripheral.find("minecraft:tconstruct_craftingstation")
-binnieStorage = peripheral.find("binnicore:tile_machine", function (_, obj) return obj.getMetadata() end)
+binnieStorage = peripheral.find("binniecore:tile_machine", filterBinnie)
 function reload()
-    network.craftyTurtle = peripheral.find("turtle", function (_, obj) return obj.getLabel() == "crafter" end)
+    network.craftyTurtle = peripheral.find("turtle", filterCrafter)
     network.craftingStation = peripheral.find("minecraft:tconstruct_craftingstation")
-    network.binnieStorage = peripheral.find("binniecore:tile_machine", function (_, obj) return obj.getMetadata().name == "binniecore:storage" end)
+    network.binnieStorage = peripheral.find("binniecore:tile_machine", filterBinnie)
 end
