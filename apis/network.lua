a5adaa8e707aa53ce764f081a9a4e9e396a721e0
yelsharawy <yelsharawy20@stuy.edu>
2021-05-17 16:34:09 -0400

    Fixed network API reloading

--- apis/network.lua
+++ apis/network.lua
@@ -1,6 +1,8 @@
+craftyTurtle = peripheral.find("turtle", function (_, obj) return obj.getLabel() == "crafter" end)
+craftingStation = peripheral.find("minecraft:tconstruct_craftingstation")
+binnieStorage = peripheral.find("binnicore:tile_machine", function (_, obj) return obj.getMetadata() end)
 function reload()
-    craftyTurtle = peripheral.find("turtle", function (_, obj) return obj.getLabel() == "crafter" end)
-    craftingStation = peripheral.find("minecraft:tconstruct_craftingstation")
-    binnieStorage = peripheral.find("binnicore:tile_machine", function (_, obj) return obj.getMetadata() end)
+    network.craftyTurtle = peripheral.find("turtle", function (_, obj) return obj.getLabel() == "crafter" end)
+    network.craftingStation = peripheral.find("minecraft:tconstruct_craftingstation")
+    network.binnieStorage = peripheral.find("binnicore:tile_machine", function (_, obj) return obj.getMetadata() end)
 end
-reload()
